3b873c1d9beb86518c6d2712b20c9e263ef0dd78
secarlosvidal <sekarlorm@gmail.com>
2020-12-02 03:43:30 +0100

    sigo adelante

--- src/main/kotlin/FicheroPrincipal.kt
+++ src/main/kotlin/FicheroPrincipal.kt
@@ -1,4 +1,13 @@
 fun main(args: Array<String>) {
     println("Hello World!")
     imprimir()
+
+    // si cambia de valor
+    var x = 7
+    var y: String = "my String"
+
+    // no cambia de valor
+    val siempre = 5
+    // ya NO puedo siempre = 9
+
 }
